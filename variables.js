bc959f25508b353090b1fc84b53a09e5d834f7e6
Thang0226 <thang.nd0226@gmail.com>
2024-09-12 10:03:16 +0700

    review file

--- variables.js
+++ variables.js
@@ -3,13 +3,13 @@ let f = 20.5
 let b = true
 let s = 'Hà Nội'
 
-document.write('i = ' + i)
+document.write(`i = ${i}`)
 document.write('<br>')
-document.write('f = ' + f)
+document.write(`f = ${f}`)
 document.write('<br>')
-document.write('b = ' + b)
+document.write(`b = ${b}`)
 document.write('<br>')
-document.write('s = ' + s)
+document.write(`s = ${s}`)
 
 let width = 20
 let height = 30
